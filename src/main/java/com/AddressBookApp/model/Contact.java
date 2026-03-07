d5aa4d7ca932b6559929aeec7295905a62fb9b1f
keshavvyadavv <keshavyadavv21@gmail.com>
2026-03-07 12:13:18 +0530

    [Keshav Yadav] : Modified Contact.java

--- src/main/java/com/AddressBookApp/model/Contact.java
+++ src/main/java/com/AddressBookApp/model/Contact.java
@@ -25,5 +25,13 @@ public class Contact {
         System.out.println(firstName + " " + lastName + " " + address + " "
                 + city + " " + state + " " + zip + " " + phoneNumber + " " + email);
     }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return  lastName;
+    }
 }
 
