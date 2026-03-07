bf847587c2c65d590d8dc5effc0774873f67efbc
keshavvyadavv <keshavyadavv21@gmail.com>
2026-03-07 11:36:52 +0530

    [Keshav Yadav] : Added UC1-ContactEntity

--- src/main/java/com/AddressBookApp/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/AddressBookApp/AddressBookApp/AddressBookAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
 		SpringApplication.run(AddressBookAppApplication.class, args);
 	}
 
