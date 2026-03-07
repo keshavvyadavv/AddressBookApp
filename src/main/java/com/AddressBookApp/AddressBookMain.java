99662ee46d131f1d29f8b56bd5a2b468702cde5b
keshavvyadavv <keshavyadavv21@gmail.com>
2026-03-07 12:12:47 +0530

    [Keshav Yadav] : Modified AddressBookMain

--- src/main/java/com/AddressBookApp/AddressBookMain.java
+++ src/main/java/com/AddressBookApp/AddressBookMain.java
@@ -7,12 +7,47 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
+
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Address Book Program");
-
         Scanner sc = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
 
+        // Add initial contact
+        System.out.println("Add a contact:");
+        Contact contact = createContact(sc);
+        addressBook.addContact(contact);
+
+        // Display contacts
+        System.out.println("Contacts before edit:");
+        addressBook.displayContacts();
+
+        // Edit contact
+        System.out.println("\nEnter name of contact to edit:");
+        System.out.print("First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Last Name: ");
+        String lastName = sc.nextLine();
+
+        System.out.println("Enter updated details:");
+        Contact updatedContact = createContact(sc);
+
+        boolean edited = addressBook.editContact(firstName, lastName, updatedContact);
+
+        if (edited) {
+            System.out.println("Contact updated successfully!");
+        } else {
+            System.out.println("Contact not found.");
+        }
+
+        // Display contacts after edit
+        System.out.println("Contacts after edit:");
+        addressBook.displayContacts();
+    }
+
+    // Helper method to create a contact from console input
+    private static Contact createContact(Scanner sc) {
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
 
@@ -37,13 +72,7 @@ public class AddressBookMain {
         System.out.print("Enter Email: ");
         String email = sc.nextLine();
 
-        Contact contact = new Contact(firstName, lastName, address,
-                city, state, zip, phone, email);
-
-        AddressBook addressBook = new AddressBook();
-
-        addressBook.addContact(contact);
-
-        addressBook.displayContacts();
+        return new Contact(firstName, lastName, address, city, state, zip, phone, email);
     }
-}
+
+    }
